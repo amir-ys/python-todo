78c93871707419de78980ca7bd1132812505db4c
amir <amir.youse74@gmail.com>
2025-05-01 12:42:58 +0330

    add login method

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-
 from dataclasses import dataclass
 import dataclasses
 import json
+from typing import Optional
 
+authenticated_user = None
 
 print("Welcome to todo app")
-command = input("Please enter your command: ")
+command = input("Please enter your command ")
+
+class Json():
+    @staticmethod
+    def from_json(json_str):
+     return User(**json.loads(json_str))
+
 
 @dataclass
-class User:
-    name: str
+class User(Json):
     email: str
     password: str
+    name: Optional[str] = None
 
 def register_user():
      
@@ -32,6 +39,43 @@ def register_user():
         f.write(user_json_data)
 
 
+def login_user():
+    email = input("please enter your email : \n")
+    password =  input("please enter your password : \n")   
+
+    input_user = User(
+        email=email,
+        password=password
+    ) 
+
+    with open("users.txt", "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            try:
+                user = User.from_json(line)
+                if user.email == input_user.email and user.password == input_user.password:
+                    global authenticated_user
+                    authenticated_user = user.email
+                    break
+            
+            except json.JSONDecodeError as e:
+                print("خطا در خواندن JSON:", e)
+        
+
+            
+    if authenticated_user is None:
+        print("authentication failed: invalid email or password.")
+        return False
+    else:
+        print(f"welcome back, {authenticated_user}!")
+        return True
+
+     
+
+
 match command :
     case "register-user" | "r" : register_user()
+    case "login" | "l" : login_user()
 
